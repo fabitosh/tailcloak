5ca574307cbab1fa34a2c809db513bb4a0840e81
Fabio Meier <fabitosh@gmail.com>
2026-06-06 21:47:25 +0200

    feat(main): invoke tailscale based on ssid trust status

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 mod config;
+mod tailscale;
+mod wifi;
 
 fn main() {
     let config = config::Config::load().expect("failed to load config");
     println!("trusted SSIDs: {:?}", config.trusted_ssids);
+    let current_ssid = wifi::get_current_ssid();
+    let is_trusted: bool = match &current_ssid {
+        Some(current_ssid) => config.trusted_ssids.contains(current_ssid),
+        None => false, // no wifi = not trusted
+    };
+
+    if is_trusted {
+        let _ = tailscale::down();
+    } else {
+        let _ = tailscale::up();
+    }
 }
